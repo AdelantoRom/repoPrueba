b92f68cb386d454503ac822a8d323fba09c715b0
Maria Paz Rivas <pazroscianorivas@gmail.com>
2025-06-08 10:53:47 -0300

    Cambios en el Navbar

--- repo-prueba/src/components/navBar/NavBar.tsx
+++ repo-prueba/src/components/navBar/NavBar.tsx
@@ -1,8 +1,15 @@
 export default function Navbar() {
   return (
-    <nav className="flex items-center justify-between px-6 py-4">
-      <div className="text-xl font-bold text-black">Rombux</div>
-      <div className="space-x-12 text-white">
+    <nav className="fixed top-0 w-full z-50 flex items-center justify-between py-[63px] px-[143px] bg-[url('/a10b28.png')] bg-cover bg-center">
+    
+      <img
+        src="/logo.png"
+        alt="Logo Rombux"
+        className="w-[240px] h-[39px]"
+      />
+
+   
+      <div className="flex items-center space-x-20 text-white text-[32px] leading-[24px] font-normal font-[Albert Sans]">
         <a href="#">Servicios</a>
         <a href="#">Casos</a>
         <a href="#">Contacto</a>
