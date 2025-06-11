a4f7246dc0f3340a4b26b24dd5689af870ef97a0
Hugo Eseverri <hugooeseverri@gmail.com>
2025-06-10 23:36:33 -0300

    cambios en enfoque

--- repo-prueba/src/components/Home/Enfoque/Enfoque.tsx
+++ repo-prueba/src/components/Home/Enfoque/Enfoque.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 function Enfoque() {
     return (
         <div className='text-black mt-[250px] mb-[265px]'>
-            <div className="w-full flex justify-center ml-2 mb-[89px]">
+            <div className="w-full flex justify-around ml-2 mb-[89px]">
                 <h3 className="text-[120px] w-[1410px] leading-none font-light text-center">Un enfoque especializado <span className="text-[#D81FB9] font-normal">con 3 objetivos clave:</span>
                 </h3>
             </div>
