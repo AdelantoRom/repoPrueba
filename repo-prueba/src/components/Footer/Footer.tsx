3902690157a0cfc1070595667e8b4a16625c296d
Maria Paz Rivas <pazroscianorivas@gmail.com>
2025-06-12 22:56:55 -0300

    Modificaciones Footer y Navbar

--- repo-prueba/src/components/Footer/Footer.tsx
+++ repo-prueba/src/components/Footer/Footer.tsx
@@ -5,7 +5,9 @@ export default function Footer() {
     <div className="relative w-full h-[277px] bg-no-repeat bg-[length:100%_100%] bg-[url('/footer.png')]">
 
       {/* Bloque izquierdo (logo + soluciones) más cerca del centro */}
-     <div className="absolute left-[320px] top-1/2 -translate-y-1/2 flex flex-col space-y-2">
+    <div className="absolute left-[180px] top-[40%] -translate-y-1/2 flex flex-col space-y-2">
+
+     
   <Image src="/logofooter.png" alt="Logo Footer" width={245} height={39} />
   <Image src="/footersoluciones.png" alt="Soluciones" width={245} height={28} />
 </div>
@@ -17,8 +19,8 @@ export default function Footer() {
       </div>
 
       {/* logoin.png (esquina derecha con margen) */}
-      <div className="absolute right-[100px] bottom-[40px]">
-        <Image src="/logoin.png" alt="Logo IN" width={80} height={80} />
+      <div className="absolute right-[180px] bottom-[125px]">
+        <Image src="/logoin.png" alt="Logo IN" width={90} height={90} />
       </div>
 
     </div>
