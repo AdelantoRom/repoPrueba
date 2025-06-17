e408a6d217de803a77babc527da8708ee79ba920
Hugo Eseverri <hugooeseverri@gmail.com>
2025-06-17 00:26:48 -0300

    corrección en sección hotel

--- repo-prueba/src/components/Home/Enfoques/Marcas/Hotel/Hotel.tsx
+++ repo-prueba/src/components/Home/Enfoques/Marcas/Hotel/Hotel.tsx
@@ -1,4 +1,3 @@
-// components/PopupModal.tsx
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -16,44 +15,27 @@ const PopupModalHotel = ({ onClose }: Props) => {
                 </button>
                 <div className=' w-[650px] mx-4'>
                     <div className='w-[610px] h-[579px] flex flex-col justify-around'>
-                        <Link href={"/"} className='text-[#747474] text-[22px] mt-6 mb-5'>Caso</Link>
-                        <h1 className="text-[36px] font-semibold mb-6 leading-[36px]">Hotel Marketing and Technology</h1>
-                        <p className="mb-4 text-[20px] font-light leading-[28px]">
+                        <Link href={"/"} className='text-[#747474] text-[22px] mt-4'>Caso</Link>
+                        <h1 className="text-[36px] font-semibold mb-4 leading-[36px] w-[319px]">Hotel Marketing and Technology</h1>
+                        <p className="mb-1 text-[20px] font-light leading-[28px]">
                             HMandT es una empresa de servicios turísticos con base en Ontario-Canadá, dedicada al marketing de hoteles y reservas.</p>
-                        <p className="mb-4 text-[20px] font-light leading-[28px]">
-                            Creamos su identidad marcaria, apelando a atributos de clara significación: el mundo y la estrella mirados desde una ventanilla o pantalla. Diseñamos su página de presentación, incluyendo las ofertas y destinos preferidos para el alojamiento, traslados y viajes.
+                        <p className="mb-1 text-[20px] font-light leading-[28px]">
+                            Creamos su identidad marcaria, apelando a atributos de clara significación: el mundo y la estrella mirados desde una ventanilla o pantalla. Diseñamos su página de presentación incluyendolas ofertas y destinos preferidos para el alojamiento, traslados y viajes.
                         </p>
-                        <p className="italic mb-10 text-[20px] font-medium leading-[28px]">
+                        <p className="italic mb-19 text-[20px] font-medium leading-[28px]">
                             En base a una alianza como Affiliate Partner de Booking.com y Rentalcars.com, integramos Book engine para el sistema automático de reservas.
                         </p>
-
-                        <Image src="/hmandt.jpg" alt="Logo Hotel" width={340} height={98} className='-mb-28' />
+                        <Image src="/hmandt.jpg" alt="Logo Hotel" width={340} height={98} className='-mb-38' />
                     </div>
                 </div>
-                <div className="relative flex content-center w-[606px] h-[679px] mt-8">
+                <div className="relative w-[606px] h-[679px] mt-8">
                     <Image
-                        src="/BannerOjo.jpg"
-                        alt="Banner Ojo"
-                        width={303}
-                        height={679}
-                        className="rounded-l-3xl"
-                    />
-                    <Image
-                        src="/BannerMano.jpg"
-                        alt="Banner Mano"
-                        width={303}
-                        height={679}
-                        className="rounded-r-3xl"
-                    />
-                    <div className="absolute bottom-0 left-1/2 -translate-x-1/2 z-10">
-                        <Image
-                            src="/HP-3.jpg"
-                            alt="Logo HP"
-                            width={341}
-                            height={268}
-                            className="object-contain"
-                        />
-                    </div>
+                        src="/hmandt2.png"
+                        alt="Hotel"
+                        width={606}
+                        height={680}
+                        className="rounded-3xl shadow-md"
+                    />      
                 </div>
             </div>
         </div>
