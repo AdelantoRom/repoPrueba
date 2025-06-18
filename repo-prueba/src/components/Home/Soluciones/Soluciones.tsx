fd9bcc85ffe69b0f44619a581a2b00356f59557a
Hugo Eseverri <hugooeseverri@gmail.com>
2025-06-18 14:53:39 -0300

    cambios varios

--- repo-prueba/src/components/Home/Soluciones/Soluciones.tsx
+++ repo-prueba/src/components/Home/Soluciones/Soluciones.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function Soluciones() {
   return (
-    <div className="bg-[url('/gradiente1.png')] bg-no-repeat bg-cover w-full min-h-screen text-white">
+    <div className="w-full min-h-screen text-white">
       <div className="flex items-start justify-between px-0 pt-[120px] pb-[40px]">
         {/* Columna de texto + formulario */}
         <div className="w-1/2 pl-[143px] pr-8 flex flex-col justify-start space-y-12">
@@ -12,40 +12,40 @@ export default function Soluciones() {
               Soluciones Digitales de Negocios
             </h2>
             <h1 className="text-[126px] font-normal leading-[132px] text-white">
-              Potenciamos<br/>tu Empresa<br/>en la Era Digital
+              Potenciamos<br />tu Empresa<br />en la Era Digital
             </h1>
           </div>
 
           {/* Formulario */}
-<div className="flex flex-col space-y-10">
-  <h3 className="text-2xl text-white mt-4">
-    ¡Anótate ahora y participa de un sorteo con <br /> importantes beneficios para tu próxima campaña!
-  </h3>
+          <div className="flex flex-col space-y-10">
+            <h3 className="text-2xl text-white mt-4">
+              ¡Anótate ahora y participa de un sorteo con <br /> importantes beneficios para tu próxima campaña!
+            </h3>
 
-  <div className="flex flex-col space-y-8 max-w-[540px]">
-    {/* Campo de Email */}
-   <div className="relative w-full">
- <div className="w-full">
-  <input
-    type="email"
-    placeholder="Email"
-    className="w-full bg-transparent border-b border-white text-white placeholder-white focus:outline-none focus:border-[#00CED1] text-[24px] leading-[32px] pb-2"
-  />
-</div>
+            <div className="flex flex-col space-y-8 max-w-[540px]">
+              {/* Campo de Email */}
+              <div className="relative w-full">
+                <div className="w-full">
+                  <input
+                    type="email"
+                    placeholder="Email"
+                    className="w-full bg-transparent border-b border-white text-white placeholder-white focus:outline-none focus:border-[#00CED1] text-[24px] leading-[32px] pb-2"
+                  />
+                </div>
 
-</div>
+              </div>
 
 
-    {/* Botón */}
-    <button
-      type="submit"
-      className="bg-white text-[24px] font-bold h-[46px] w-[186px] rounded-[50px]"
-      style={{ color: "#19B7CC" }}
-    >
-      Enviar
-    </button>
-  </div>
-</div>
+              {/* Botón */}
+              <button
+                type="submit"
+                className="bg-white text-[24px] font-bold h-[46px] w-[186px] rounded-[50px]"
+                style={{ color: "#19B7CC" }}
+              >
+                Enviar
+              </button>
+            </div>
+          </div>
 
         </div>
 
@@ -62,7 +62,7 @@ export default function Soluciones() {
       </div>
 
       <div className="px-[143px] py-20 flex justify-between items-start">
-       
+
         <div className="flex flex-col space-y-6 w-1/2 max-w-[629px]">
           <h3 className="text-[32px] leading-[40px] font-medium">Transformación Digital</h3>
           <h4 className="text-[24px] leading-[40px] font-normal">
@@ -79,7 +79,7 @@ export default function Soluciones() {
           </h4>
         </div>
 
-     
+
         <div className="w-1/2 flex justify-end">
           <h2 className="text-[120px] leading-[120px] font-normal text-right max-w-[829px]">
             Impulsando tus Negocios al Futuro
@@ -87,18 +87,18 @@ export default function Soluciones() {
         </div>
       </div>
 
-     <div className="w-full relative px-[143px] pb-20 pt-10">
-  <div className="flex flex-wrap justify-center items-center gap-x-17 gap-y-32 mb-12">
-    <Image src="/HP blanco.png" alt="Imagen Ho" width={130} height={76} className="object-contain" />
-    <Image src="/DowAgro.png" alt="Imagen Dow Agro" width={216} height={33} className="object-contain" />
-    <Image src="/SuperPollo blanco.png" alt="Imagen SuperPollo" width={122} height={101} className="object-contain" />
-    <Image src="/Amarula blanco.png" alt="Imagen Amarula" width={100} height={68} className="object-contain" />
-    <Image src="/Granix blanco.png" alt="Imagen Granix" width={122} height={81} className="object-contain" />
-    <Image src="/The Clinic blanco.png" alt="Imagen The Clinic" width={121} height={36} className="object-contain" />
-    <Image src="/Advanta blanco.png" alt="Imagen Advanta" width={152} height={53} className="object-contain" />
-    <Image src="/Bi blanco.png" alt="Imagen Bi" width={59} height={58} className="object-contain" />
-  </div>
-</div>
+      <div className="w-full relative px-[143px] pb-20 pt-10">
+        <div className="flex flex-wrap justify-center items-center gap-x-17 gap-y-32 mb-12">
+          <Image src="/HP blanco.png" alt="Imagen Ho" width={130} height={76} className="object-contain" />
+          <Image src="/DowAgro.png" alt="Imagen Dow Agro" width={216} height={33} className="object-contain" />
+          <Image src="/SuperPollo blanco.png" alt="Imagen SuperPollo" width={122} height={101} className="object-contain" />
+          <Image src="/Amarula blanco.png" alt="Imagen Amarula" width={100} height={68} className="object-contain" />
+          <Image src="/Granix blanco.png" alt="Imagen Granix" width={122} height={81} className="object-contain" />
+          <Image src="/The Clinic blanco.png" alt="Imagen The Clinic" width={121} height={36} className="object-contain" />
+          <Image src="/Advanta blanco.png" alt="Imagen Advanta" width={152} height={53} className="object-contain" />
+          <Image src="/Bi blanco.png" alt="Imagen Bi" width={59} height={58} className="object-contain" />
+        </div>
+      </div>
 
     </div>
   );
