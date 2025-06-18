78706e4ea0f3267d3ae76dcc22ab0476d1675fcf
Hugo Eseverri <hugooeseverri@gmail.com>
2025-06-18 15:12:32 -0300

    cambios en navbar

--- repo-prueba/src/components/Footer/Footer.tsx
+++ repo-prueba/src/components/Footer/Footer.tsx
@@ -1,14 +1,17 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Footer() {
   return (
     <div className="relative w-[1920px] h-[277px] bg-no-repeat bg-[length:100%_100%] bg-[url('/footer.png')] mx-auto">
       
       {/* Bloque izquierdo (logo + soluciones) */}
+      <Link href="/">
       <div className="absolute left-[139px] bottom-[109px] flex flex-col">
         <Image src="/Logo Rombux fucsia.png" alt="Logo Footer" width={245} height={39} />
         <Image className="-mt-5" src="/footersoluciones.png" alt="Soluciones" width={245} height={28} />
       </div>
+      </Link>
 
       {/* Imagen derechos centrada horizontalmente */}
       <div className="absolute left-268 bottom-[109px] -translate-x-1/2 text-[18px] w-[1255px]">
