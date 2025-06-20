a5bc61a4e55805cff079be0d5ff6eb8fb9a661b5
Hugo Eseverri <hugooeseverri@gmail.com>
2025-06-20 01:52:17 -0300

    cambios en benchmarking

--- repo-prueba/src/app/branding/Branding.tsx
+++ repo-prueba/src/app/branding/Branding.tsx
@@ -5,13 +5,13 @@ export default function Benchmarking() {
   return (
     // ⬇️ CONTENEDOR QUE BLOQUEA ESPACIO Y AÍSLA LOS ELEMENTOS ABSOLUTOS
     <div className="relative w-full"> 
-      <div className="relative flex flex-col ml-[140px] pt-[100px] space-y-8">
+      <div className="relative flex flex-col ml-[137px] pt-[100px] space-y-8">
         {/* Título */}
         <div>
-          <h2 className="mt-64  font-bold text-[64px] leading-[120px] text-[#D81FB9]">
+          <h2 className="mt-[265px]  font-bold text-[64px] leading-[120px] text-[#D81FB9]">
             Branding
           </h2>
-          <p className="font-light italic text-[36px] leading-[48px] text-[#000000]">
+          <p className="font-light italic text-[36px] leading-[48px] mt-[1px] -ml-[1px] text-[#000000]">
             Construye una marca con impulso y<br />
             reputación.
           </p>
@@ -24,21 +24,22 @@ export default function Benchmarking() {
             alt="Ellipse"
             width={23}
             height={23}
-            className="absolute top-[-155px] left-[760px]"
+            className="absolute top-[-165px] left-[811px]"
           />
           <Image
             src="/Line 7.png"
             alt="Line"
-            width={565}
+            width={605}
             height={2}
-            className="absolute top-[-145px] left-[760px]"
+            className="absolute top-[-155px] left-[822px]"
           />
           <Image
             src="/Image 6.png"
             alt="Image 6"
-            width={750}
-            height={750}
-            className="absolute top-[-530px] left-[900px]"
+            width={800}
+            height={800}
+            className="absolute top-[-565px] left-[966px]"
+            unoptimized
           />
         </div>
 
@@ -49,7 +50,7 @@ export default function Benchmarking() {
             alt="Branding"
             width={135}
             height={135}
-            className="absolute top-[-570px] left-[700px]"
+            className="absolute top-[-570px] left-[755px]"
           />
         </div>
       </div>
