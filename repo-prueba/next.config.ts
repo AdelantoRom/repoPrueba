45d2c60f5112ee5f8218a464f2b600255ad22994
Hugo Eseverri <hugooeseverri@gmail.com>
2025-06-25 00:26:10 -0300

    cambios para producción

--- repo-prueba/next.config.ts
+++ repo-prueba/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   eslint: {
-    ignoreDuringBuilds: true, // 🔥 evita que ESLint rompa el build
+    ignoreDuringBuilds: true,
   },
 };
 
