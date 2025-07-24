7bbfb739155f4355ca5f64e8b1e4906b98a0a5b1
Hugo Eseverri <hugooeseverri@gmail.com>
2025-07-24 16:06:38 -0300

    cambios

--- repo-prueba/src/app/layout.tsx
+++ repo-prueba/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Albert_Sans } from "next/font/google";
 import "./globals.css";
-import Navbar from "../components/NavBar/NavBar"
+import Navbar from "@/components/NavBar/NavBar";
 
 
 
