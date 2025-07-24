0213e39fc989964bdb13cc5e59e4a819d2060eb5
Hugo Eseverri <hugooeseverri@gmail.com>
2025-07-24 15:59:42 -0300

    cambios

---
+++ repo-prueba/src/app/bintelligenz/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import BinMobile from '@/components/Home/Enfoques/Marcas/Bintelligenz.tsx/BinMobile'
+
+function page() {
+    return (
+        <div><BinMobile /></div>
+    )
+}
+
+export default page
