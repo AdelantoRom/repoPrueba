f985defe78211401a67bd132181a2ce29a394bb8
Hugo Eseverri <hugooeseverri@gmail.com>
2025-07-27 02:11:58 -0300

    cambios en about us, full responsive

--- repo-prueba/src/components/BotonesInteractivos/Botones.tsx
+++ repo-prueba/src/components/BotonesInteractivos/Botones.tsx
@@ -5,12 +5,12 @@ function Botones() {
     return (
         <div>
             <div className="w-full flex justify-center lg:ml-4 lg:translate-x-[8px] 2xl:ml-6 2xl:translate-x-[-64px] 2xl:mt-[-16px]">
-              
-              <div className="flex justify-center items-start gap-x-4 lg:gap-x-46 2xl:gap-x-110 w-full lg:max-w-[900px] 2xl:max-w-[1343px]">
+
+                <div className="flex justify-center items-start max-sm:flex-col max-sm:items-center max-sm:gap-y-10 gap-x-4 lg:gap-x-46 2xl:gap-x-110 w-full lg:max-w-[900px] 2xl:max-w-[1343px]">
 
 
-            {/* Botón 1 */}
-<div className="flex flex-col items-center lg:w-[132px] lg:translate-x-[-32px] 2xl:w-[160px] 2xl:translate-x-[-34px]">
+                    {/* Botón 1 */}
+                    <div className="flex flex-col items-center lg:w-[132px] lg:translate-x-[-32px] 2xl:w-[160px] 2xl:translate-x-[-34px]">
 
                         <Image
                             src="/Calendly.gif"
@@ -20,7 +20,7 @@ function Botones() {
                             className="rounded-lg lg:w-[64px] lg:h-[65px] 2xl:h-[48px] 2xl:w-[48px]"
                         />
                         <div className="mt-[24px] text-center font-medium text-white text-[18px] leading-[28px] w-[203px] 
-                         lg:text-[20px] lg:leading-[24px] lg:w-[132px] lg:h-[46px]">
+                        lg:text-[20px] lg:leading-[24px] lg:w-[132px] lg:h-[46px]">
                             Agenda una <br />reunión
                         </div>
                     </div>
@@ -50,7 +50,7 @@ function Botones() {
                             className="rounded-lg lg:w-[64px] lg:h-[65px]"
                         />
                         <div className="mt-[24px] text-center font-medium text-white  text-[18px] leading-[28px] w-[203px] 
-                         lg:text-[20px] lg:leading-[24px] lg:w-[203px] lg:h-[46px]">
+                        lg:text-[20px] lg:leading-[24px] lg:w-[203px] lg:h-[46px]">
                             Chatea con Romy, nuestro agente IA
                         </div>
                     </div>
