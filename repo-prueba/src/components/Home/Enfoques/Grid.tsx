727f7066b790c30b36b93c271460bb7fe38e9ec7
Hugo Eseverri <hugooeseverri@gmail.com>
2025-07-28 01:28:47 -0300

    cambios varios para responsive

--- repo-prueba/src/components/Home/Enfoques/Grid.tsx
+++ repo-prueba/src/components/Home/Enfoques/Grid.tsx
@@ -44,9 +44,11 @@ export default function Grid() {
                     <ButtonBintelligenz />
                 </div>
             </div>
-            <div className="lg:hidden">
+            <div className="lg:hidden flex items-center justify-center bg-[#F4F0F0] px-4 w-full">
+
                 <CarouselMarcas />
             </div>
+
         </div>
     );
 }
