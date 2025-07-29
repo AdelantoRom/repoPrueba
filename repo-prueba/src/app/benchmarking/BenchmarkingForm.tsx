f2b0239627e653019c07bfd0d9a326a24343e90c
Hugo Eseverri <hugooeseverri@gmail.com>
2025-07-29 14:57:56 -0300

    textos form responsive

--- repo-prueba/src/app/benchmarking/BenchmarkingForm.tsx
+++ repo-prueba/src/app/benchmarking/BenchmarkingForm.tsx
@@ -16,35 +16,35 @@ export default function BenchmarkingForm() {
           Benchmarking:
         </h2>
         <p className="font-light text-[64px] leading-[64px] text-center text-white max-lg:text-[40px] max-lg:leading-[40px]">
-          orientado a resultados
+          orientado a <br className="sm:hidden"/> resultados
         </p>
       </div>
 
 
-      <div className="w-full max-w-[1145px] flex items-center justify-center ml-6">
-        <h3 className="text-[24px] leading-[28px] text-center text-white max-lg:text-[20px]">
-          Somos un grupo de profesionales especializados en <br className="2xl:hidden"/> escalar
-          empresas y startups, unidos bajo una misma <br className="2xl:hidden"/> cultura: lograr el
-          liderazgo desde una sólida propuesta <br className="2xl:hidden"/> de valor.
+      <div className="w-full max-w-[1145px] flex items-center justify-center ml-6 max-sm:ml-0">
+        <h3 className="text-[24px] leading-[28px] max-sm:w-[336px] text-center text-white max-lg:text-[20px]">
+          Somos un grupo de profesionales especializados en <br className="2xl:hidden max-sm:hidden"/> escalar
+          empresas y startups, unidos bajo una misma <br className="2xl:hidden max-sm:hidden"/> cultura: lograr el
+          liderazgo desde una sólida propuesta <br className="2xl:hidden max-sm:hidden"/> de valor.
         </h3>
       </div>
 
 
-      <div className="mt-[132px] relative ml-[15px] max-lg:mt-[100px]">
+      <div className="mt-[132px] relative ml-[15px] max-lg:mt-[100px] max-sm:ml-0">
         <Image
           src="/Group-155.png"
           alt="Decoración Benchmarking"
           width={563}
           height={130}
           unoptimized
-          className="max-lg:w-[479px]"
+          className="max-lg:w-[479px] max-sm:w-[336px]"
         />
         <Image
           src="/logoin.png"
           alt="Logo In"
           width={35}
           height={35}
-          className="absolute top-[81px] left-[109px] max-lg:top-[70px] max-lg:left-[90px] max-lg:w-[30px] max-lg:h-[30px]"
+          className="absolute top-[81px] left-[109px] max-lg:top-[70px] max-lg:left-[90px] max-lg:w-[30px] max-lg:h-[30px] max-sm:w-[25px] max-sm:h-[25px] max-sm:top-[50px] max-sm:left-[60px]"
         />
       </div>
 
