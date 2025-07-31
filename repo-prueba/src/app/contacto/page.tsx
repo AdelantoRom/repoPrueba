914b84c30e46b8979638e82bf4464b7eb1929e08
Maria Paz Rivas <pazroscianorivas@gmail.com>
2025-07-31 15:52:07 -0300

    mejoras contacto

--- repo-prueba/src/app/contacto/page.tsx
+++ repo-prueba/src/app/contacto/page.tsx
@@ -6,20 +6,28 @@ export default function Page() {
   return (
     <div className="flex flex-col min-h-screen">
       <main className="flex-grow">
+        
         <div className="flex flex-col pt-[100px] gap-[40px] lg:flex-row lg:items-start lg:ml-[60px] lg:pt-[180px] lg:gap-[60px] xl:ml-[110px] 2xl:ml-[15%] 2xl:pt-[260px]">
 
           {/* Left Column */}
-          <div className="w-full max-w-[640px] lg:w-[500px] 2xl:w-[1100px] px-4">
-            <h1 className="text-black font-light text-[32px] leading-[36px] w-full mt-[50px] mb-[30px] lg:text-[48px] lg:leading-[53px] lg:mb-[40px] lg:mt-[12px] 2xl:text-[80px] 2xl:leading-[80px] 2xl:mb-[94px] 2xl:mt-[10px]">
-              Iniciemos tu <br className="2xl:hidden" />
-              <span className="hidden 2xl:inline"> </span>
-              liderazgo digital
-            </h1>
+          <div className="w-full lg:basis-[60%] lg:flex-grow 2xl:basis-[70%] 2xl:max-w-none px-4">
+            
+            {/* h1 y h2 agrupados */}
+            
+           <div className="flex flex-col space-y-[30px] lg:space-y-[100px]">
+  <h1 className="mt-[40px] lg:mt-[4px] w-full break-words text-black font-light text-[32px] leading-[36px] lg:text-[70px] lg:leading-[70px] lg:font-light 2xl:text-[80px] 2xl:leading-[80px] 2xl:font-normal">
+  <span>Iniciemos tu</span>
+  <span className="block lg:inline"> liderazgo digital</span>
+</h1>
 
-            <h2 className="text-[#D81FB9] text-[24px] leading-[24px] font-semibold w-fit mt-[20px] lg:w-[351px] lg:text-[48px] lg:leading-[53px] lg:font-normal lg:mt-[30px] 2xl:text-[48px] 2xl:leading-[48px] 2xl:font-normal">
-              Contáctanos
-            </h2>
 
+  <h2 className="text-[#D81FB9] text-[24px] leading-[24px] font-normal w-fit lg:w-[351px] lg:text-[48px] lg:leading-[53px] lg:font-normal 2xl:text-[48px] 2xl:leading-[48px] 2xl:font-normal">
+    Contáctanos
+  </h2>
+</div>
+
+
+            {/* Formulario */}
             <form className="mt-[22px] flex flex-col space-y-[18px]">
               {["Nombre y Apellido*", "Email*", "Telefono*", "Empresa*"].map((placeholder, idx) => (
                 <input
@@ -31,7 +39,7 @@ export default function Page() {
               ))}
 
               {/* Checkbox Area */}
-              <div className="w-full border border-[#707070] rounded-[5px] pt-[18px] pl-6 space-y-4 bg-white h-[320px] lg:h-[340px] lg:w-[605px] 2xl:w-[638px]">
+              <div className="w-full border border-[#707070] rounded-[5px] pt-[18px] pl-6 space-y-4 bg-white h-[320px] lg:h-[280px] 2xl:h-[265px] lg:w-[605px] 2xl:w-[638px]">
                 <p className="text-[18px] leading-[36px] font-semibold text-[#4B4B4B] mb-[10px]">
                   Área/s de servicios requeridos*
                 </p>
@@ -74,7 +82,7 @@ export default function Page() {
               </div>
 
               {/* Submit Button */}
-              <div className="w-full flex justify-start mb-4 lg:w-[605px] 2xl:w-[638px] 2xl:mb-12">
+              <div className="w-full flex justify-start -mb-4 lg:w-[605px] lg:mb-12 2xl:w-[638px] 2xl:mb-[70px]">
                 <button
                   type="submit"
                   className="bg-[#D81FB9] text-white text-[24px] leading-[32px] font-semibold w-[192px] h-[48px] rounded-[50px] mt-[29px] mb-0 text-center"
@@ -87,49 +95,75 @@ export default function Page() {
           </div>
 
           {/* Right Column */}
-          <div className="flex flex-col lg:translate-x-[80px] xl:translate-x-[120px] 2xl:translate-x-[200px]">
-            <div className="hidden lg:block relative lg:w-[400px] lg:h-[400px] lg:ml-[20px] xl:w-[700px] 2xl:h-[700px] 2xl:ml-[-41%]">
-              <Image
-                src="/grupoesfera+halo.png"
-                alt="Grupo Formulario"
-                width={700}
-                height={700}
-                className="absolute top-[45%] lg:top-[87%] lg:left-[55%] xl:left-[65%] 2xl:top-[69%] 2xl:left-[90%] z-0 transform -translate-x-[65%] -translate-y-1/2 object-cover rounded-full lg:w-[360px] lg:h-[320px] xl:h-[450px] xl:w-[450px] 2xl:w-[680px] 2xl:h-[680px]"
-              />
-              <Image
-                src="/Trama esfera celeste.png"
-                alt="Trama Formulario"
-                width={505}
-                height={505}
-                className="absolute top-[45%] lg:top-[87%] lg:left-[53%] xl:left-[63%] 2xl:top-[69%] 2xl:left-[88%] z-10 transform -translate-x-[65%] -translate-y-1/2 xl:h-[350px] xl:w-[350px] 2xl:w-[520px] 2xl:h-[520px] lg:w-[300px] lg:h-[300px] animate-spin [animation-direction:reverse] [animation-duration:120s]"
-              />
+          <div className="flex flex-col flex-shrink-0 relative lg:translate-x-[-50px] xl:translate-x-[120px] 2xl:translate-x-[200px]">
+
+            <div className="hidden lg:block relative lg:w-[400px] lg:h-[400px] lg:ml-[20px] xl:w-[700px] 2xl:h-[700px] 2xl:ml-[-65%] -translate-y-[40px] lg:translate-y-[55px] xl:-translate-y-[100px] 2xl:-translate-y-[120px]">
+            <Image
+  src="/grupoesfera+halo.png"
+  alt="Grupo Formulario"
+  width={700}
+  height={700}
+  className="absolute top-[90%] 2xl:top-[85%] 2xl:left-[75%] z-0 transform -translate-x-[65%] -translate-y-1/2 object-cover rounded-full w-[680px] h-[680px]"
+/>
+
+<Image
+  src="/Trama esfera celeste.png"
+  alt="Trama Formulario"
+  width={505}
+  height={505}
+  className="absolute top-[90%] 2xl:top-[85%] 2xl:left-[73%] z-10 transform -translate-x-[65%] -translate-y-1/2 w-[520px] h-[520px] animate-spin [animation-direction:reverse] [animation-duration:120s]"
+/>
+
+
             </div>
 
-            <div className="mt-0 w-full max-w-[640px] mb-14 px-4 flex flex-col items-start lg:mt-[405px] lg:ml-8 xl:ml-[100px] 2xl:ml-[120px] 2xl:mt-[240px] lg:px-0">
-              <a href="mailto:info@rombux.com">
-                <p className="text-[20px] leading-[32px] font-semibold cursor-pointer mt-[20px] mb-12">
-                  <span className="text-[#707070]">Email:</span> <span className="text-[#D81FB9]">info@rombux.com</span>
-                </p>
-              </a>
+           {/* Email / Oficina - Mobile */}
+<div className="block lg:hidden w-full max-w-[640px] px-4 mt-10">
+  <a href="mailto:info@rombux.com">
+    <p className="text-[20px] leading-[32px] font-semibold cursor-pointer mt-[4px] mb-6">
+      <span className="text-[#707070]">Email:</span> <span className="text-[#D81FB9]">info@rombux.com</span>
+    </p>
+  </a>
 
-              <div className="w-full text-[#707070]">
-                <h3 className="text-[20px] leading-[28px] font-semibold mb-4">Oficina:</h3>
-                <p className="text-[18px] leading-[28px] font-normal">
-                  Lezica 4363,<br />
-                  Ciudad de Buenos Aires<br />
-                  (1202AAI) Argentina
-                </p>
-              </div>
+  <div className="w-full text-[#707070]">
+    <h3 className="text-[20px] leading-[28px] font-semibold mb-6">Oficina:</h3>
+    <p className="text-[18px] leading-[28px] font-normal mb-[50px]">
+      Lezica 4363,<br />
+      Ciudad de Buenos Aires<br />
+      (1202AAI) Argentina
+    </p>
+  </div>
 
-              <div className="hidden lg:block mt-10">
-                <Image
-                  src="/logoin.png"
-                  alt="Logo institucional"
-                  width={46}
-                  height={46}
-                />
-              </div>
-            </div>
+  
+</div>
+
+{/* Email / Oficina - Tablet & Desktop */}
+<div className="hidden lg:flex absolute w-full max-w-[640px] px-4 flex-col items-start top-[100%] translate-y-[340px] xl:translate-y-[100px] 2xl:translate-y-[150px] lg:ml-8 xl:ml-[100px] 2xl:ml-[-150px] lg:px-0">
+  <a href="mailto:info@rombux.com">
+    <p className="text-[20px] leading-[32px] font-semibold cursor-pointer mt-[20px] mb-12">
+      <span className="text-[#707070]">Email:</span> <span className="text-[#D81FB9]">info@rombux.com</span>
+    </p>
+  </a>
+
+  <div className="w-full text-[#707070]">
+    <h3 className="text-[20px] leading-[28px] font-semibold mb-4">Oficina:</h3>
+    <p className="text-[18px] leading-[28px] font-normal">
+      Lezica 4363,<br />
+      Ciudad de Buenos Aires<br />
+      (1202AAI) Argentina
+    </p>
+  </div>
+
+  <div className="mt-10">
+    <Image
+      src="/logoin.png"
+      alt="Logo institucional"
+      width={46}
+      height={46}
+    />
+  </div>
+</div>
+
           </div>
 
         </div>
@@ -141,6 +175,154 @@ export default function Page() {
     </div>
   );
 }
+
+
+// import React from "react";
+// import Image from "next/image";
+// import Footer from "@/components/Footer/Footer";
+
+// export default function Page() {
+//   return (
+//     <div className="flex flex-col min-h-screen">
+//       <main className="flex-grow">
+//         <div className="flex flex-col pt-[100px] gap-[40px] lg:flex-row lg:items-start lg:ml-[60px] lg:pt-[180px] lg:gap-[60px] xl:ml-[110px] 2xl:ml-[15%] 2xl:pt-[260px]">
+
+//           {/* Left Column */}
+//           <div className="w-full lg:basis-[60%] lg:flex-grow 2xl:basis-[70%] 2xl:max-w-none px-4">
+//             <h1 className="w-full break-words text-black font-light text-[32px] leading-[36px] mt-[50px] mb-[30px] lg:text-[70px] lg:leading-[70px] lg:mb-[40px] lg:font-light lg:mt-[12px] 2xl:text-[80px] 2xl:leading-[80px] 2xl:font-semibold 2xl:mb-[94px] 2xl:mt-[10px]">
+//               Iniciemos tu liderazgo <br className="hidden 2xl:block" />
+//               digital
+//             </h1>
+
+//             <h2 className="text-[#D81FB9] text-[24px] leading-[24px] font-semibold w-fit mt-[20px] lg:w-[351px] lg:text-[48px] lg:leading-[53px] lg:font-normal lg:mt-[50px] 2xl:text-[48px] 2xl:leading-[48px] 2xl:font-normal">
+//               Contáctanos
+//             </h2>
+
+//             <form className="mt-[22px] flex flex-col space-y-[18px]">
+//               {["Nombre y Apellido*", "Email*", "Telefono*", "Empresa*"].map((placeholder, idx) => (
+//                 <input
+//                   key={idx}
+//                   type="text"
+//                   placeholder={placeholder}
+//                   className="w-full h-[41px] border border-[#707070] rounded-[5px] font-medium text-[16px] leading-[24px] placeholder:text-gray-600 text-[#4B4B4B] bg-white pl-[24px] lg:w-[605px] 2xl:w-[638px]"
+//                 />
+//               ))}
+
+//               {/* Checkbox Area */}
+//               <div className="w-full border border-[#707070] rounded-[5px] pt-[18px] pl-6 space-y-4 bg-white h-[320px] lg:h-[280px] 2xl:h-[265px] lg:w-[605px] 2xl:w-[638px]">
+//                 <p className="text-[18px] leading-[36px] font-semibold text-[#4B4B4B] mb-[10px]">
+//                   Área/s de servicios requeridos*
+//                 </p>
+//                 {[
+//                   { bold: 'Benchmarking', rest: ' / Investigación de mercado y propuesta de valor.' },
+//                   { bold: 'Branding', rest: ' / Identidad, presencia digital, reputación.' },
+//                   { bold: 'Marketing Digital', rest: ' / Conexión y adquisición de clientes.' },
+//                   { bold: 'Growth', rest: ' / Crecimiento y posicionamiento de mercado.' },
+//                   { bold: 'Data + IA', rest: ' / Información clave y automatización de procesos.' },
+//                 ].map(({ bold, rest }, idx) => (
+//                   <label key={idx} className="flex items-start space-x-[20px] text-[14px] leading-[16px] text-[#4B4B4B]">
+//                     <input
+//                       type="checkbox"
+//                       className="
+//                         w-[23px] h-[23px] border-2 border-[#707070] rounded-[5px] appearance-none bg-white
+//                         checked:bg-[#D81FB9] checked:border-[#D81FB9]
+//                         checked:after:content-['✔'] checked:after:text-white checked:after:text-[14px]
+//                         checked:after:flex checked:after:items-center checked:after:justify-center
+//                         lg:w-[24px] lg:h-[23px]
+//                         2xl:w-[24px] 2xl:h-[24px]
+//                         focus-visible:ring-2 focus-visible:ring-[#D81FB9]
+//                       "
+//                     />
+//                     <span>
+//                       <strong>{bold}</strong>{rest}
+//                     </span>
+//                   </label>
+//                 ))}
+//               </div>
+
+//               {/* Textarea */}
+//               <div className="w-full h-[113px] border border-[#707070] rounded-[5px] p-3 bg-white mt-0 lg:w-[605px] lg:h-[212px] 2xl:w-[638px]">
+//                 <textarea
+//                   id="mensaje"
+//                   name="mensaje"
+//                   required
+//                   className="w-full h-full pl-3 pt-3 text-[14px] leading-[36px] font-semibold placeholder:text-gray-500 text-[#4B4B4B] resize-none focus:outline-none bg-transparent"
+//                   placeholder="¿Por qué tema nos consultas?*"
+//                 />
+//               </div>
+
+//               {/* Submit Button */}
+//               <div className="w-full flex justify-start mb-4 lg:w-[605px] 2xl:w-[638px] 2xl:mb-12">
+//                 <button
+//                   type="submit"
+//                   className="bg-[#D81FB9] text-white text-[24px] leading-[32px] font-semibold w-[192px] h-[48px] rounded-[50px] mt-[29px] mb-0 text-center"
+//                   style={{ textAlign: 'center' }}
+//                 >
+//                   Enviar
+//                 </button>
+//               </div>
+//             </form>
+//           </div>
+
+//           {/* Right Column */}
+//           <div className="flex flex-col flex-shrink-0 relative lg:translate-x-[-20px] xl:translate-x-[120px] 2xl:translate-x-[200px]">
+
+//             <div className="hidden lg:block relative lg:w-[400px] lg:h-[400px] lg:ml-[20px] xl:w-[700px] 2xl:h-[700px] 2xl:ml-[-65%] -translate-y-[40px] lg:-translate-y-[15px] xl:-translate-y-[100px] 2xl:-translate-y-[120px]">
+//               <Image
+//                 src="/grupoesfera+halo.png"
+//                 alt="Grupo Formulario"
+//                 width={700}
+//                 height={700}
+//                 className="absolute top-[45%] lg:top-[87%] lg:left-[45%] xl:left-[55%] 2xl:top-[69%] 2xl:left-[75%] z-0 transform -translate-x-[65%] -translate-y-1/2 object-cover rounded-full lg:w-[360px] lg:h-[320px] xl:h-[450px] xl:w-[450px] 2xl:w-[680px] 2xl:h-[680px]"
+//               />
+//               <Image
+//                 src="/Trama esfera celeste.png"
+//                 alt="Trama Formulario"
+//                 width={505}
+//                 height={505}
+//                 className="absolute top-[45%] lg:top-[87%] lg:left-[43%] xl:left-[53%] 2xl:top-[69%] 2xl:left-[73%] z-10 transform -translate-x-[65%] -translate-y-1/2 xl:h-[350px] xl:w-[350px] 2xl:w-[520px] 2xl:h-[520px] lg:w-[300px] lg:h-[300px] animate-spin [animation-direction:reverse] [animation-duration:120s]"
+//               />
+//             </div>
+
+//             {/* Email / Oficina Block */}
+//             <div className="absolute w-full max-w-[640px] px-4 flex flex-col items-start top-[100%] translate-y-[-40px] lg:translate-y-[250px] xl:translate-y-[100px] 2xl:translate-y-[150px] lg:ml-8 xl:ml-[100px] 2xl:ml-[-150px] lg:px-0">
+
+//               <a href="mailto:info@rombux.com">
+//                 <p className="text-[20px] leading-[32px] font-semibold cursor-pointer mt-[20px] mb-12">
+//                   <span className="text-[#707070]">Email:</span> <span className="text-[#D81FB9]">info@rombux.com</span>
+//                 </p>
+//               </a>
+
+//               <div className="w-full text-[#707070]">
+//                 <h3 className="text-[20px] leading-[28px] font-semibold mb-4">Oficina:</h3>
+//                 <p className="text-[18px] leading-[28px] font-normal">
+//                   Lezica 4363,<br />
+//                   Ciudad de Buenos Aires<br />
+//                   (1202AAI) Argentina
+//                 </p>
+//               </div>
+
+//               <div className="hidden lg:block mt-10">
+//                 <Image
+//                   src="/logoin.png"
+//                   alt="Logo institucional"
+//                   width={46}
+//                   height={46}
+//                 />
+//               </div>
+//             </div>
+//           </div>
+
+//         </div>
+//       </main>
+
+//       <footer>
+//         <Footer />
+//       </footer>
+//     </div>
+//   );
+// }
+
 
 
 // import React from "react";
