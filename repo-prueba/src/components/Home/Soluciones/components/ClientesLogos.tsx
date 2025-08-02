65987d12feb98cbff2cb7665a6cc4d0be27d21bb
Hugo Eseverri <hugooeseverri@gmail.com>
2025-08-02 20:36:54 -0300

    cambios en logos clientes

--- repo-prueba/src/components/Home/Soluciones/components/ClientesLogos.tsx
+++ repo-prueba/src/components/Home/Soluciones/components/ClientesLogos.tsx
@@ -5,18 +5,18 @@ export default function ClientesLogos() {
     <div className="w-full flex justify-center pt-6">
       {/* MOBILE */}
       
-      <div className="grid grid-cols-4 place-items-center gap-y-4 gap-x-2 mt-6 w-full px-0 lg:hidden border-amber-500">
+      <div className="grid grid-cols-4 place-items-center gap-y-10 gap-x-20 mt-6 w-full px-0 lg:hidden border-amber-500 mb-5 max-md:gap-x-10 max-sm:gap-x-0">
         <div className="relative w-[52px] h-[27px]">
-          <Image src="/hpblanco.png" alt="HP" fill className="object-contain" />
+          <Image src="/hpblanco.png" alt="HP" width={52} height={27} className="object-contain" />
         </div>
         <div className="relative w-[83px] h-[12px]">
-          <Image src="/DowAgro blanco.png" alt="Dow Agro" fill className="object-contain" />
+          <Image src="/DowAgro blanco.png" alt="Dow Agro" width={83} height={12} className="object-contain" />
         </div>
         <div className="relative w-[35px] h-[28px]">
-          <Image src="/Group.png" alt="Super Pollo" fill className="object-contain" />
+          <Image src="/Group.png" alt="Super Pollo" width={35} height={28} className="object-contain" />
         </div>
         <div className="relative w-[41px] h-[28px]">
-          <Image src="/Amarula blanco.png" alt="Amarula" fill className="object-contain" />
+          <Image src="/Amarula blanco.png" alt="Amarula" width={41} height={28} className="object-contain" />
         </div>
         <div className="relative w-[47px] h-[13px]">
           <Image src="/The Clinic blanco.png" alt="The Clinic" fill className="object-contain" />
@@ -34,16 +34,15 @@ export default function ClientesLogos() {
         {/* TABLET & DESKTOP: desde lg+ */}
       <div
         className="
-          hidden lg:flex flex-row items-center justify-center
-          w-[919px] h-[60px] gap-x-14 -mt-10 mb-6 pl-8
-          lg:mb-6
-          2xl:w-[1645px] 2xl:h-[56px] 2xl:gap-x-30 2xl:mt-44 2xl:mb-16 2xl:-ml-[10px]
+          hidden lg:flex  justify-between items-center
+          h-[60px] -mt-10 mb-6 mx-25
+          lg:mb-10 w-full
+          2xl:h-[56px] gap-x-10 2xl:mx-30 2xl:mt-44 2xl:mb-16 
         "
       >
 
         <div className="relative lg:w-[73px] lg:h-[45px] 2xl:w-[96px] 2xl:h-[56px]">
-
-          <Image src="/hpblanco.png" alt="Imagen HP" width={96} height={56} className="object-contain" />
+          <Image src="/hpblanco.png" alt="Imagen HP" width={96} height={56} className="" />
         </div>
         <div className="relative lg:w-[121px] lg:h-[19px] 2xl:w-[167px] 2xl:h-[25px]">
           <Image src="/DowAgroSciences.svg" width={164} height={25} alt="Imagen Dow Agro"  className="object-contain" />
